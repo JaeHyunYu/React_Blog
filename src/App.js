130d5f80b37bded9dcfdd866afb20e9b7f958f96
YJH <slade12307@gmail.com>
2022-05-18 07:35:42 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -4,32 +4,31 @@ import {useState} from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집'; // 서버에서 가져온 데이터로 가정
-  let [a,b]=useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
-  let [logo,setLogo]=useState('ReactBlog');
+ 
+  let [title,b]=useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
+  let [like,setLike]=useState(0);
+
   return (
     <div className="App">
      <div className='black-nav'>
-       <h4 style={{color:'red',fontSize:'16px'}}>{post}</h4>
+       <h4 style={{fontSize:'16px'}}>ReactBlog</h4>
      </div>
+
      <div className='list'>
-       <h4>{a[0]}</h4>
+       <h4>{title[0]} <span onClick={()=>{setLike(like+1)}}>👍</span> {like} </h4>
        <p>2월 17일</p>
      </div>
-     <div className='black-nav'>
-       <h4 style={{color:'red',fontSize:'16px'}}>{post}</h4>
-     </div>
+
      <div className='list'>
-       <h4>{a[1]}</h4>
+       <h4>{title[1]}</h4>
        <p>2월 17일</p>
      </div>
-     <div className='black-nav'>
-       <h4 style={{color:'red',fontSize:'16px'}}>{post}</h4>
-     </div>
+
      <div className='list'>
-       <h4>{a[2]}</h4>
+       <h4>{title[2]}</h4>
        <p>2월 17일</p>
      </div>
+
     </div>
   );
 }
